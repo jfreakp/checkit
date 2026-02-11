d37b63cc32509cd86024ec129264b7266ff39f02
Juan Pablo Torres <jptorresdota@gmail.com>
2026-02-11 14:48:53 -0500

    Se arregla el nav pueda coger para la web de escritorio.

--- components/navbar/NavBar.tsx
+++ components/navbar/NavBar.tsx
@@ -13,30 +13,44 @@ export const NavBar = () => {
     const [open, setOpen] = useState(false);
 
     return (
-        <div className="md:hidden w-full">
-            <button
-                className="p-2 focus:outline-none w-full bg-white border-b border-gray-200 justify-end flex"
-                onClick={() => setOpen(!open)}
-                aria-label="Abrir menú"
-            >
-                <div className="flex flex-col items-center">
-                    <span className="block w-6 h-0.5 bg-black mb-1"></span>
-                    <span className="block w-6 h-0.5 bg-black mb-1"></span>
-                    <span className="block w-6 h-0.5 bg-black"></span>
-                </div>
-            </button>
-            {open && (
-                <nav className="absolute left-0 top-[56px] w-full flex flex-col gap-4 bg-white shadow z-50 p-4">
+        <div>
+            <div className="md:hidden w-full">
+                <button
+                    className="p-2 focus:outline-none w-full bg-white border-b border-gray-200 justify-end flex"
+                    onClick={() => setOpen(!open)}
+                    aria-label="Abrir menú"
+                >
+                    <div className="flex flex-col items-center">
+                        <span className="block w-6 h-0.5 bg-black mb-1"></span>
+                        <span className="block w-6 h-0.5 bg-black mb-1"></span>
+                        <span className="block w-6 h-0.5 bg-black"></span>
+                    </div>
+                </button>
+                {open && (
+                    <nav className="absolute left-0 top-[56px] w-full flex flex-col gap-4 bg-white shadow z-50 p-4">
+                        {navItems.map((item) => (
+                            <ActiveLink
+                                key={item.path}
+                                path={item.path}
+                                text={item.text}
+                                onClick={() => setOpen(false)}
+                            />
+                        ))}
+                    </nav>
+                )}
+            </div>
+            <div>
+                {/* ✅ DESKTOP */}
+                <nav className="hidden md:flex gap-6 items-center justify-center bg-white h-14">
                     {navItems.map((item) => (
                         <ActiveLink
                             key={item.path}
                             path={item.path}
                             text={item.text}
-                            onClick={() => setOpen(false)}
                         />
                     ))}
                 </nav>
-            )}
+            </div>
         </div>
     )
 }
